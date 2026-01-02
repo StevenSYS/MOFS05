abf41dac29ec7a769e3cfe7ef6fa0950478baa8f
StevenSYS <139715581+StevenSYS@users.noreply.github.com>
2026-01-02 18:05:06 +0000

    [...] Added license info to the source files

--- src/mofs05.h
+++ src/mofs05.h
@@ -1,3 +1,27 @@
+/*
+	MIT License
+	
+	Copyright (c) 2026 StevenSYS
+	
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+	
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+*/
+
 #ifndef __MOFS05__
 #define __MOFS05__
 
@@ -6,11 +30,13 @@
 
 /* Macros */
 #define MOFS05_NAME "MOFS05"
-#define MOFS05_MAGIC MOFS05_NAME
-#define MOFS05_PREFIX MOFS05_NAME
+#define MOFS05_VERSION "1.0pre"
 
+#define MOFS05_MAGIC MOFS05_NAME
 #define MOFS05_LENGTH_MAGIC 6
 #define MOFS05_LENGTH_NAME 100
+
+#define MOFS05_PREFIX MOFS05_NAME
 
 /* Structs */
 typedef struct {
